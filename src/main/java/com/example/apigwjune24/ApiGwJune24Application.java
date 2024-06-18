30d7fc41c8c16aa494920201bcfbf6d44732d5dc
Bhavik <bhavik@MacBook-Pro-4.local>
2024-06-18 10:05:26 +0530

    Initial commit

---
+++ src/main/java/com/example/apigwjune24/ApiGwJune24Application.java
@@ -0,0 +1,13 @@
+package com.example.apigwjune24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGwJune24Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGwJune24Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/apigwjune24/ApiGwJune24ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apigwjune24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGwJune24ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
